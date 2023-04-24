e937b4e944609f41c1add7fdf92580e358f66cd8
madomaga <marcin.domagala@capgemini.com>
2023-04-24 14:26:09 +0200

    uploadimage

--- src/util/uploadImage.js
+++ src/util/uploadImage.js
@@ -3,19 +3,19 @@ import {
   CLOUDINARY_API_KEY,
   CLOUDINARY_UPLOAD_PRESETS,
 } from "@env";
-
 export async function uploadImage(image) {
-  const url = `https://api.cloudinary.com/v1_1/${CLOUDINARY_CLOUD_NAME}/auto/upload`;
+  const url = `https://api.cloudinary.com/v1_1/${CLOUDINARY_CLOUD_NAME}/image/upload`;
   console.log("URL: ", url);
   console.log("IMAGE: ", image);
   const formData = new FormData();
   formData.append("file", image);
-  formData.append("upload_preset", "da6yzf6d");
+  formData.append("upload_preset", CLOUDINARY_UPLOAD_PRESETS);
   const response = await fetch(url, {
     method: "POST",
     body: formData,
+  }).then((response) => {
+    console.log("RESPONSE: ", response);
   });
-  console.log("RESPONSE: ", response);
 
   if (!response.ok) {
     throw new Error("Failed to fetch adress!");
