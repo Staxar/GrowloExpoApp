9e96ab3bc2ef2040e4252ca6add65b8163370b82
madomaga <marcin.domagala@capgemini.com>
2023-04-25 15:24:31 +0200

    Change firebaseConfig

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,26 +1,37 @@
+import {
+  Firebase_apiKey,
+  Firebase_authDomain,
+  Firebase_projectId,
+  Firebase_storageBucket,
+  Firebase_messagingSenderId,
+  Firebase_appId,
+  Firebase_measurementId,
+  Firebase_databaseURL,
+} from "@env";
 // Import the functions you need from the SDKs you need
 import { getDatabase } from "@firebase/database";
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getStorage } from "firebase/storage";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 export const firebaseConfig = {
-  apiKey: "AIzaSyAcP00dy-UXp01oUFH0RQQfOLRDRGrE-i8",
-  authDomain: "growlo-38066.firebaseapp.com",
-  projectId: "growlo-38066",
-  storageBucket: "growlo-38066.appspot.com",
-  messagingSenderId: "488931900046",
-  appId: "1:488931900046:web:6d73747ef319a1c065a7df",
-  measurementId: "G-C224BGMCYQ",
-  databaseURL:
-    "https://growlo-38066-default-rtdb.europe-west1.firebasedatabase.app/",
+  apiKey: Firebase_apiKey,
+  authDomain: Firebase_authDomain,
+  projectId: Firebase_projectId,
+  storageBucket: Firebase_storageBucket,
+  messagingSenderId: Firebase_messagingSenderId,
+  appId: Firebase_appId,
+  measurementId: Firebase_measurementId,
+  databaseURL: Firebase_databaseURL,
 };
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getDatabase();
+const storage = getStorage(app);
 
-export { auth, db };
+export { auth, db, storage };
