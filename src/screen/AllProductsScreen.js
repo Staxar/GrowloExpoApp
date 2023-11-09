97a46c38d3c20e34f98208fabffb36ddd12b0027
madomaga <marcin.domagala@capgemini.com>
2023-11-09 12:55:42 +0100

    New all items

--- src/screen/AllProductsScreen.js
+++ src/screen/AllProductsScreen.js
@@ -1,33 +1,66 @@
-import { useCallback, useEffect, useState } from "react";
-import { FlatList, SafeAreaView } from "react-native";
-import { StyleSheet, View } from "react-native";
+import React, { useCallback, useEffect, useState } from "react";
+import {
+  FlatList,
+  SafeAreaView,
+  StyleSheet,
+  View,
+  Text,
+  RefreshControl,
+} from "react-native";
 import ProductCard from "../components/ui/ProductCard";
-import { Text } from "react-native";
 import { Typography } from "../constans/styles";
 import { getProducts } from "../util/getProducts";
-import { RefreshControl } from "react-native";
-import { ScrollView } from "react-native";
 
 export default function AllProductsScreen({ route, navigation }) {
   const [data, setData] = useState([]);
   const [category, setCategory] = useState("");
   const [refreshing, setRefreshing] = useState(false);
+  const [page, setPage] = useState(1);
+  const [loading, setLoading] = useState(false);
+  const [hasMore, setHasMore] = useState(true);
 
   const onRefresh = useCallback(() => {
     setRefreshing(true);
+    setHasMore(true);
+    setPage(1);
     setRefreshing(false);
   }, []);
+
+  const loadMore = async () => {
+    if (loading || !hasMore) return;
+
+    setLoading(true);
+    setPage(page + 1);
+
+    const newData = await getProducts(page)
+      .then((res) => res)
+      .catch((e) => {
+        console.error(e);
+        return [];
+      });
+
+    if (newData.length === 0) {
+      setHasMore(false);
+    } else {
+      // Filter out existing items to maintain uniqueness
+      const uniqueNewData = newData.filter(
+        (newItem) =>
+          !data.some((existingItem) => existingItem.id === newItem.id)
+      );
+
+      // Update state with unique new items
+      setData((prevData) => [...prevData, ...uniqueNewData]);
+    }
+
+    setLoading(false);
+  };
 
   useEffect(() => {
     setData([]);
     setCategory(route.params.itemParams);
-    const getData = async () => {
-      await getProducts()
-        .then((res) => setData(res))
-        .catch((e) => console.error(e));
-    };
-
-    getData();
+    setHasMore(true);
+    setPage(1);
+    loadMore();
   }, [navigation, route, refreshing]);
 
   const filteredProducts = data.filter((product) =>
@@ -46,12 +79,11 @@ export default function AllProductsScreen({ route, navigation }) {
         {filteredProducts && (
           <FlatList
             data={filteredProducts}
-            onEndReachedThreshold={0.1}
             numColumns={2}
             renderItem={({ item }) => (
               <ProductCard
                 productName={item.title}
-                productPrize={item.prize}
+                productPrice={item.price}
                 productUnit={item.unit}
                 productAmount={item.amount}
                 productId={item.id}
@@ -63,6 +95,8 @@ export default function AllProductsScreen({ route, navigation }) {
             refreshControl={
               <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
             }
+            onEndReached={loadMore}
+            onEndReachedThreshold={0.1}
           />
         )}
       </View>
